da268288d8514bcb6326a7f411b5b4318eb0d6cc
Heather Shultz <silverlycold@gmail.com>
2022-03-23 23:01:56 -0500

    first assignment with featuring:
    the window.alert() and document.write() methods.
    
    Assigned string values to variables and concatenated the variables together.
    
    A color assigned to a variable using the fontcolor() method.
    
    Comments throughout JavaScript explaining code

---
+++ Basic JavaScript Projects/Project1_exprssions_alert/JS/main.js
@@ -0,0 +1,18 @@
+
+
+function getInputValue() {
+
+    // Selecting the input element and get its value 
+    let inputVal = document.getElementById("inputId").value;
+
+    // assigned varables  for response
+    let first = "Nice to meet you, ";
+    let last = "!!";
+
+     // Displaying the value with green font
+    document.write(first + inputVal.fontcolor("green") + last);
+
+    //alerting with a greeting
+    alert("Hi " + inputVal);
+   
+  }
