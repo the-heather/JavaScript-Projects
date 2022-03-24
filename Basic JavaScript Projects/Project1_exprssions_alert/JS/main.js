c8d11a628301d0e0171ac4c282f53c7debc03c43
Heather Shultz <silverlycold@gmail.com>
2022-03-24 11:54:42 -0500

    2nd Assignment featuring:
    a function that uses the += operator
    
    An HTML element to call and display the function
    
    Comments throughout JavaScript explaining code

--- Basic JavaScript Projects/Project1_exprssions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_exprssions_alert/JS/main.js
@@ -2,17 +2,15 @@
 
 function getInputValue() {
 
-    // Selecting the input element and get its value 
-    let inputVal = document.getElementById("inputId").value;
+  // Selecting the input element and get its value 
+  let inputVal = document.getElementById("inputId").value;
 
-    // assigned varables  for response
-    let first = "Nice to meet you, ";
-    let last = "!!";
+  // assigned varables  for response
+  let first = "Nice to meet you, ";
+  let last = "!!";
 
-     // Displaying the value with green font
-    document.write(first + inputVal.fontcolor("green") + last);
+    // Displaying the value with green font
+  document.write(first + inputVal.fontcolor("green") + last);
 
-    //alerting with a greeting
-    alert("Hi " + inputVal);
-   
-  }
+}
+
