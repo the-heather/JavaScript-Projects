7244d6bc6d7c93c78603fbdf1307c308533c02e5
Heather Shultz <silverlycold@gmail.com>
2022-03-24 19:45:06 -0500

    3rd JavaScript Assignment

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,40 @@
+{function add() {
+    var addition = 389 + 494;
+    document.getElementById("Math").innerHTML = addition;
+}
+
+function sub() {
+    var subtraction = 534 - 265;
+    document.getElementById("Math2").interHTML = subtraction;
+}
+
+function mult() {
+    var multiplication = 485 * 593;
+    document.getElementById("Math3").interHTML =  multiplication;
+}
+
+function div() {
+    var division = 580 / 5;
+    document.getElementById("Math4").interHTML =  division;
+}
+
+function more_math() {
+    var all = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("Math5").innerHTML =  all;
+}
+
+function modulus() {
+    var remainder = 25 % 6;
+    document.getElementById("Math6").innerHTML = remainder;
+}
+
+function negation() {
+    var x = 10;
+    document.getElementById("math7").innerHTML = -x;
+}}
+
+var X = 5.25;
+X--;
+document.write(X);
+
+window.alert(Math.random());
